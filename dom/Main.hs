5e5d85a2e5634d542eb693c88de3e7000ef46f5d
Nikolay Yakimov <root@livid.pp.ru>
2016-10-16 19:30:30 +0300

    Switch to toggle minimization
    Since minimization is exponential, it can take ungodly amounts of time
    on larger FD sets, when it's not strictly required in most cases.

--- dom/Main.hs
+++ dom/Main.hs
@@ -7,6 +7,7 @@ import qualified Data.Set as S
 
 import FDTools
 import Data.List
+import Control.Arrow
 
 import Reflex.Spider.Internal
 
@@ -15,12 +16,15 @@ import GHCJS.DOM.Document
 import GHCJS.DOM.Types (castToHTMLDocument, castToHTMLElement)
 import Util
 
-graphInput :: MonadWidget t m => m (Dynamic t String)
+graphInput :: MonadWidget t m => m (Dynamic t (String, Bool))
 graphInput = do
   n <- textArea def
+  c <- checkbox False def
+  text "Строгая минимизация (медленно!)"
   el "br" $ return ()
   bt <- button "Анализ"
-  holdDyn "" $ tagDyn (value n) bt
+  v <- combineDyn (,) (value n) (value c)
+  holdDyn ("", False) $ tagDyn v bt
 
 widget :: Widget
             Spider
@@ -38,7 +42,7 @@ widget w =
 
 main :: IO ()
 main = widget $ el "div" $ do
-  inp <- mapDyn parseGraph =<< graphInput
+  inp <- mapDyn (first parseGraph) =<< graphInput
   _ <- dyn =<< mapDyn outputWidget inp
   return ()
 
@@ -56,16 +60,18 @@ customBlock header disp = do
   disp
 
 outputWidget :: (Show a, MonadWidget t m) =>
-                Either a Graph -> m ()
-outputWidget (Left err) = el "div" $ text $ show err
-outputWidget (Right inp) = do
+                (Either a Graph, Bool) -> m ()
+outputWidget (Left err, _) = el "div" $ text $ show err
+outputWidget (Right inp, trueMinimize) = do
   let
+    (minimize', minhead)
+      | trueMinimize = (minimize, "Минимальный список ФЗ")
+      | otherwise = (collect . nontrivial, "Минимальный список ФЗ (неточный)")
     full = fullext inp
     -- nt = nontrivial full
     -- sk = superkeys full
     pk = potkeys full
-    -- minf = conservative minimize inp
-    minf = conservative (collect . nontrivial) inp
+    minf = conservative minimize' inp
     nf3l = nf3 minf
     nfbcl = nfbc minf
     normalized = normalize minf
@@ -73,19 +79,21 @@ outputWidget (Right inp) = do
   -- simpleBlock "Полный список ФЗ (кроме тривиальных)" nt
   -- simpleBlock "Суперключи" sk
   simpleBlock "Потенциальные ключи" pk
-  simpleBlock "Минимальный список ФЗ" minf
+  simpleBlock minhead minf
   simpleBlock "ФЗ, не удовлетворяющие 3НФ (минимальное множество)" nf3l
   simpleBlock "ФЗ, не удовлетворяющие НФБК (минимальное множество)" nfbcl
   case normalized of
     Left norm -> do
       el "h1" $ text $ show "Некорректная нормализация!"
-      normWidget minf norm
-    Right norm -> normWidget minf norm
+      normWidget minf norm trueMinimize
+    Right norm -> normWidget minf norm trueMinimize
 
 normWidget :: MonadWidget t m =>
-              Graph -> [[Vertex]] -> m ()
-normWidget inp norm = do
-  let prj = map (\x -> (x, minimize . flip project (fullext inp) . S.fromList $ x)) norm
+              Graph -> [[Vertex]] -> Bool -> m ()
+normWidget inp norm trueMinimize = do
+  let prj
+        | trueMinimize = map (\x -> (x, minimize . flip project (fullext inp) . S.fromList $ x)) norm
+        | otherwise = map (\x -> (x, collect . nontrivial . flip project inp . S.fromList $ x)) norm
   customBlock "Нормализованные отношения" $
     el "pre" $ do
       let
